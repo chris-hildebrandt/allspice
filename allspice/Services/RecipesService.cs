f183afe1d88e7058e90457c1dd1416fdcbda492a
chris-hildebrandt <ch.hild15@gmail.com>
2022-09-18 23:32:37 -0600

    get favs by profile in service

--- allspice/Services/RecipesService.cs
+++ allspice/Services/RecipesService.cs
@@ -8,17 +8,21 @@ namespace allspice.Services
   public class RecipesService
   {
     private readonly RecipesRepository _recipesRepo;
-    public RecipesService(RecipesRepository recipesRepo) {
-        _recipesRepo = recipesRepo;
+    public RecipesService(RecipesRepository recipesRepo)
+    {
+      _recipesRepo = recipesRepo;
     }
-    internal List<Recipe> GetAllRecipes(){
+    internal List<Recipe> GetAllRecipes()
+    {
       return _recipesRepo.GetAllRecipes();
+    }
+    internal Recipe GetRecipeById(int id)
+    {
+      Recipe recipe = _recipesRepo.GetRecipeById(id);
+      if (recipe == null)
+      {
+        throw new Exception("That recipe id does not exist");
       }
-    internal Recipe GetRecipeById(int id){
-        Recipe recipe = _recipesRepo.GetRecipeById(id);
-        if(recipe == null){
-            throw new Exception("That recipe id does not exist");
-        }
       return recipe;
     }
     internal List<Recipe> GetRecipesByProfileId(string profileId)
@@ -26,7 +30,7 @@ namespace allspice.Services
       List<Recipe> recipes = _recipesRepo.GetRecipesByProfileId(profileId);
       return recipes;
     }
-        internal List<Recipe> GetFavoriteRecipesByProfileId(string profileId)
+    internal List<Recipe> GetFavoriteRecipesByProfileId(string profileId)
     {
       List<Recipe> recipes = _recipesRepo.GetFavoriteRecipesByProfileId(profileId);
       return recipes;
@@ -51,7 +55,8 @@ namespace allspice.Services
     internal string DeleteRecipe(Account userInfo, int recipeId)
     {
       Recipe recipe = GetRecipeById(recipeId);
-      if (recipe.CreatorId != userInfo.Id) {
+      if (recipe.CreatorId != userInfo.Id)
+      {
         throw new Exception("You are not authorized to delete this recipe");
       }
       _recipesRepo.DeleteRecipe(recipe.Id);
